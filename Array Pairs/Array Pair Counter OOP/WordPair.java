0470bf41c690ae530d27c8f610c0fb73afb09f96
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-10-14 15:23:51 -0400

    Update WordPair.java

--- Array Pairs/Array Pair Counter OOP/WordPair.java
+++ Array Pairs/Array Pair Counter OOP/WordPair.java
@@ -1,4 +1,15 @@
- class WordPair {
+/*
+*
+* @author devarshi
+* @since 2020-10-10
+*
+*  WordPair constructor takes in 2 string params
+*  and assigns them to local private string instance variables.
+*  WordPair objects have access to the String methods getFirst and getSecond
+*  that returns either the first element of a string pair, or the second.
+*/
+
+class WordPair {
      private String word1;
      private String word2;
 
