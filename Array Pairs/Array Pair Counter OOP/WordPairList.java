9222fc230b4bf3496be273afc7137afd54b06cc8
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-10-14 15:20:40 -0400

    Update WordPairList.java

--- Array Pairs/Array Pair Counter OOP/WordPairList.java
+++ Array Pairs/Array Pair Counter OOP/WordPairList.java
@@ -1,23 +1,36 @@
+/*
+* @author devarshi
+* @since 2020-10-10
+*
+* Creates word pairs from array and traverses through list with pairs
+* and counts number of matched pairs (pairs that have 2 of the same strings)
+*/
+
+
 import java.util.*;
+
 
  public class WordPairsList
  {
-     private ArrayList<WordPair> allPairs;
+     private ArrayList<WordPair> allPairs;	//private instance list
 
+     // constructor
      public WordPairsList(String[] words)
      {
-         // WRITE YOUR CODE HERE
          // initialize allPairs to an empty ArrayList of WordPair objects
-		 allPairs = new ArrayList<WordPair>();
+	 allPairs = new ArrayList<WordPair>();
+	     
          // nested loops through the words array to add each pair to allPairs
          for(int i=0; i < words.length; i++){
              for(int j=i+1; j < words.length; j++){
                  allPairs.add( new WordPair(words[i], words[j]) );
               }
          }
-
      }
 
+	 
+     // loops through allPairs list and checks if pair elements are identical strings (increments count)
+     // WordPair object 'a' uses getFirst() and getSecond() to check for matched pairs of elements. See WordPair file
      public int numMatches()
      {
          int count = 0;
@@ -29,17 +42,20 @@ import java.util.*;
          return count;
      }
 
+
+     //to String method	 
      public String toString() {
          return allPairs.toString();
      }
 
 
+     //Main method creates 'list' object and outputs list elements and number of matches
      public static void main(String[] args)
      {
          String[] words = {"Hi", "there", "Tyler", "Sam"};
          WordPairsList list = new WordPairsList(words);
          System.out.println(list);
-         // For second part below, uncomment this test:
+
          System.out.println("The number of matched pairs is: " + list.numMatches());
      }
  }
